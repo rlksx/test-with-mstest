3e418391826a61a86df05a85b1923ad4dac931c5
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-03-29 17:23:41 -0300

    feat: ordenitem

--- good/Store.Domain/Entities/OrdenItem.cs
+++ good/Store.Domain/Entities/OrdenItem.cs
@@ -1,6 +1,18 @@
 namespace Store.Domain.Entities;
 
-public class OrdenItem
+public class OrdenItem : Entity
 {
+   public OrdenItem(int qunatity, Product product)
+   {
+      this.Quantity = qunatity; 
+      this.Price = product != null ? product.Price : 0;
+   }
 
+   public decimal Price { get; private set; }
+   public Product Product { get; private set; }
+   public int Quantity { get; private set; }
+
+   public decimal Total() 
+      => Price * Quantity;
+     
 }
