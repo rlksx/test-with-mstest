8b701db89c256421496496b653b3ff5039341c77
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-03-29 23:45:59 -0300

    feat: eordersattus

--- good/Store.Domain/EOrderStatus.cs
+++
@@ -1,7 +0,0 @@
-namespace Store.Domain;
-public enum EOrderStatus
-{
-   AwaittingPyament = 1,
-   WaittingDekivery = 2,
-   Cancelled = 3
-}
---
+++ good/Store.Domain/Enums/EOrderStatus.cs
@@ -0,0 +1,8 @@
+namespace Store.Domain.Enums;
+
+public enum EOrderStatus
+{
+   WaittingPayment = 1,
+   WaittingDelivery = 2,
+   Canceled = 3,
+}
