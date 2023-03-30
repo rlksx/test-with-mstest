bf78a471b89d5b7cfad78c6d5828442ff10bc8cc
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-03-30 12:47:30 -0300

    feat: test order 2/12

--- good/Store.Tests/Entities/OrderTests.cs
+++ good/Store.Tests/Entities/OrderTests.cs
@@ -5,19 +5,24 @@ using Store.Domain.Entities;
 [TestClass]
 public class OrderTests
 {
+   private readonly Customer _customer = new Customer("Order Test", "ordertest@gmail.com");
+   private readonly Product _product = new Product("Produto 1", 10, true);
+   private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
+
    [TestMethod]
    [TestCategory("Domain")]
    public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
    {
-      var pedido = new Order(new Customer("test", "test@gmail.com"), 10, null);
-      Assert.AreEqual(8, pedido.Number.Length);
+      var order = new Order(_customer, 10, null);
+      Assert.AreEqual(8, order.Number.Length);
    }
 
    [TestMethod]
    [TestCategory("Domain")]
    public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
    {
-      Assert.Fail();
+      var order = new Order(_customer, 10, null);
+      Assert.AreEqual(EOrderStatus.WaittingPayment, order.Status);
    }
 
    [TestMethod]
